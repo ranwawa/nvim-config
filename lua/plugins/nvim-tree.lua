396659023334720b220224a77046ae01318bcea6
ranqirong <274544338@qq.com>
2026-04-23 20:52:29 +0800

    auto: sync nvim config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -28,6 +28,29 @@ return {
         vim.keymap.set({'n', 'i'}, '<leader>e', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true, desc = "Toggle NvimTree" })
         -- 查找当前文件在目录树中的位置
         vim.keymap.set({'n', 'i'}, '<leader>f', '<cmd>NvimTreeFindFile<CR>', { noremap = true, silent = true, desc = "Find file in NvimTree" })
+
+        -- 当 nvim-tree 是最后一个窗口时自动退出
+        vim.api.nvim_create_autocmd("QuitPre", {
+            callback = function()
+                local tree_wins = {}
+                local floating_wins = {}
+                local wins = vim.api.nvim_list_wins()
+                for _, w in ipairs(wins) do
+                    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+                    if bufname:match("NvimTree_") ~= nil then
+                        table.insert(tree_wins, w)
+                    end
+                    if vim.api.nvim_win_get_config(w).relative ~= '' then
+                        table.insert(floating_wins, w)
+                    end
+                end
+                if #wins - #floating_wins - #tree_wins == 1 then
+                    for _, w in ipairs(tree_wins) do
+                        vim.api.nvim_win_close(w, true)
+                    end
+                end
+            end,
+        })
     end,
     -- 启动时默认打开目录树
     init = function()
