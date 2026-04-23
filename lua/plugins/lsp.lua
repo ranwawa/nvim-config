6df41c8bc1a0f9b49442ddf80f921d38e7878677
ranqirong <274544338@qq.com>
2026-04-23 20:49:29 +0800

    auto: sync nvim config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,14 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { noremap = true, silent = true, buffer = args.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  end,
+})
+
+vim.lsp.enable("ts_ls")
+
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lspconfig = require("lspconfig")
-
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      end
-
-      lspconfig.ts_ls.setup({
-        on_attach = on_attach,
-        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-      })
-    end,
-  },
+  { "neovim/nvim-lspconfig" },
 }
